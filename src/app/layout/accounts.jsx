3427db7a0d484979adf7b09580046ce1f1bc1a94
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-12-29 11:38:09 +0300

    added axios

--- src/app/layout/accounts.jsx
+++ src/app/layout/accounts.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import accountsService from "../services/accounts.service";
 
+const formatter = Intl.NumberFormat("ru");
+
 function Accounts() {
     const [assets, setAssets] = useState([]);
     useEffect(async () => {
@@ -8,16 +10,18 @@ function Accounts() {
         setAssets(data);
     }, []);
     return (
-        <ul className="grid gap-3 p-2 list-none">
+        <ul className="grid m-5 list-none rounded-md bg-slate-100 shadow-sm shadow-slate-300">
             {assets.map((asset) => (
                 <li
                     key={asset._id}
-                    className=" p-3  rounded-md shadow-md shadow-gray-400"
+                    className="p-3 border-slate-300 border-b last:border-b-0"
                 >
-                    <span className="text-2xl text-gray-700">{asset.name}</span>
-                    <div className="grid grid-cols-3 mt-1">
+                    <span className="text-base text-gray-700">
+                        {asset.name}
+                    </span>
+                    <div className="grid grid-cols-3 mt-1 text-xs">
                         <span>Today: </span>
-                        <span>{asset.value}</span>
+                        <span>{formatter.format(asset.amount)}</span>
                     </div>
                 </li>
             ))}
--- src/app/services/accountTypes.service.js
+++ src/app/services/accountTypes.service.js
@@ -1,8 +1,10 @@
+import httpService from "./http.service";
 import localStorageService from "./localStorage.service";
 
 const fetchAll = async () => {
-    const data = localStorageService.getAccountTypes();
-    return { data };
+    const response = await httpService.get("DDgHZ_wgJbG8/data");
+    // const data = localStorageService.getAccountTypes();
+    return response;
 };
 
 export default { fetchAll };
