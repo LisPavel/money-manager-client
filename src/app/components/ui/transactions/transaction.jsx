2bd40916a1a13b9fd973ac6849efbd41d225dd5a
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2023-01-13 15:34:52 +0300

    transactions by month

--- src/app/components/ui/transactions/transaction.jsx
+++ src/app/components/ui/transactions/transaction.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import { ListItem } from "../../common/list";
 import TransactionInfo from "./transactionInfo";
+import { DateTime } from "luxon";
 
 const TransactionItem = ({ data, ...rest }) => {
+    const date = DateTime.fromMillis(data.date);
     return (
         <ListItem className="p-3 border-slate-300 transactions-list-item flex justify-between">
             <div>
-                <span className="text-base font-semibold">{data.title}</span>
-                <div className="flex justify-between mt-1 text-xs items-center">
-                    <small>{data.description}</small>
+                <div className="text-base font-semibold">{data.title}</div>
+                <div className="text-sm">{data.description}</div>
+                <div className="text-xs">
+                    {date.setLocale("ru").toLocaleString(DateTime.DATE_SHORT)}
                 </div>
             </div>
             <TransactionInfo {...rest} data={data} />
--- src/app/services/transactions.service.js
+++ src/app/services/transactions.service.js
@@ -1,8 +1,11 @@
+import { orderBy } from "lodash";
 import httpService from "./http.service";
 
 const fetchAll = async () => {
     // const data = localStorageService.getAssets();
     const response = await httpService.get("rxV7NDmUTaDG/data");
+
+    response.data = orderBy(response.data, "date", "desc");
     return response;
 };
 
