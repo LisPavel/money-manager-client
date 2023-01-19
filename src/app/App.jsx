060597d46af7b236704fc9c8fe8f42495bbb7153
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2023-01-19 13:52:33 +0300

    provide app loader

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { Route, Switch } from "react-router";
+import AppLoader from "./components/ui/hoc/appLoader";
 import NavBar from "./components/ui/navBar";
 import routes from "./router";
 
 function App() {
     return (
-        <div className="h-screen bg-slate-50 flex flex-col">
-            <NavBar items={routes} />
-            <div className="container mx-auto  overflow-hidden">
-                <Switch>
-                    {/* <Route component={Main} path="/" exact={true} />
+        <AppLoader>
+            <div className="h-screen bg-slate-50 flex flex-col">
+                <NavBar items={routes} />
+                <div className="container mx-auto  overflow-hidden">
+                    <Switch>
+                        {/* <Route component={Main} path="/" exact={true} />
                     <Route component={Income} path="/income" /> */}
-                    {Object.keys(routes).map((key) => (
-                        <Route {...routes[key]} key={key} />
-                    ))}
-                </Switch>
+                        {Object.keys(routes).map((key) => (
+                            <Route {...routes[key]} key={key} />
+                        ))}
+                    </Switch>
+                </div>
             </div>
-        </div>
+        </AppLoader>
     );
 }
 
---
+++ src/app/components/ui/hoc/appLoader.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    getAccountsLoadingStatus,
+    loadAccountsList,
+} from "../../../store/accounts";
+
+const AppLoader = ({ children }) => {
+    const dispatch = useDispatch();
+    const isLoading = useSelector(getAccountsLoadingStatus());
+    console.log(isLoading);
+    useEffect(() => {
+        dispatch(loadAccountsList());
+    }, []);
+    if (isLoading) return "Loading...";
+    return children;
+};
+
+export default AppLoader;
